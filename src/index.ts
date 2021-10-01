8d99aa314936fc10c6a35937e69d8d65aa65a9b9
Felipe Valencia <felipe@ocmiwc.com>
2021-10-01 02:18:09 -0500

    minor changs

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
 import CreditCardForm from './components/CreditCardForm'
+import LibraryContext from './LibraryContext'
+import {
+  cardNumberFormatter,
+  expirationDateFormatter,
+} from './utils/formatters'
+
 export { default as Button } from './components/Button'
 export { default as Card } from './components/Card'
 
 import { FormModel, CardFields } from './types'
 
-export { CardFields, FormModel }
+export {
+  LibraryContext,
+  CardFields,
+  FormModel,
+  cardNumberFormatter,
+  expirationDateFormatter,
+}
+
 export default CreditCardForm
